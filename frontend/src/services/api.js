05f75f1b56ac7db8106462d6a9f5b04cec7f7ef2
hetal517 <chavdahetal2211@gmail.com>
2026-07-14 11:11:26 +0530

    connect frontend to backend railway

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://blog-backend-production-306e.up.railway.app/api"
 });
 
 API.interceptors.request.use((config) => {
